3211e68852c526a61edb4ec8dd04ab4856ef19be
Pablo Cristeff <pablo.cristeff@gmail.com>
2025-04-13 10:30:57 -0300

    agregando figura svg con animaciones y mejora en funcion para mostrar animacion en pag historia

--- scriptshistoria.js
+++ scriptshistoria.js
@@ -1,11 +1,10 @@
-
 function estaDentroPantalla(elemento) {
     const rect = elemento.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 && 
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom > 0 &&
+        rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+        rect.right > 0
     );
 }
 /*
